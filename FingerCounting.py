557560f3635ea9e83ea10330c46d07727adb8a55
Mustafa Pamukçu <mustafa.pamukcu@titra.com.tr>
2024-12-17 21:29:11 +0300

    conflict çıksın

--- FingerCounting.py
+++ FingerCounting.py
@@ -63,4 +63,4 @@ while True:
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-    #Buraya yeni kodlar ekledim.Buraya bir daha bir şeyler ekledim.
+    #Buraya yeni kodlar ekledim.Buraya bir daha bir şeyler ekledim. Conflict çıkarmaya çalışıyorum.
